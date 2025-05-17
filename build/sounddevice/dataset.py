09f00c7662bc23f7e520c557361dff8d11168fd2
Fadel Akram <af8356207@gmail.com>
2025-05-17 07:02:27 -0700

    finished setting up the TDNN neural network

--- build/sounddevice/dataset.py
+++ build/sounddevice/dataset.py
@@ -2,6 +2,8 @@ from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
 from  keras.api.utils import to_categorical
 from keras.api.preprocessing.sequence import pad_sequences
+from keras import layers, models
+model = models.load_model('tdnn_speaker_model.h5')
 from train import build_tdnn_model
 from process  import load_and_preprocess
 from mfcc_extraction import extract_mfcc
@@ -40,3 +42,18 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 model.fit(X_padded, y_categorical, epochs=10, batch_size=32)
 model.save('tdnn_speaker_model.h5')
 # model = tf.keras.models.load_model('tdnn_speaker_model.h5')
+embedding_model = models.Model(inputs=model.input, outputs=model.layers[-2].output)
+speaker_embeddings = {}
+for speaker in labels:
+    speaker_path = os.path.join('dataset', speaker)
+    embeddings = []
+    for file in os.listdir(speaker_path):
+        if file.endswith('.wav'):
+            file_path = os.path.join(speaker_path, file)
+            y_audio, sr = load_and_preprocess(file_path)
+            mfcc = extract_mfcc(y_audio, sr)
+            mfcc_padded = pad_sequences([mfcc], maxlen=input_shape[0], padding='post', dtype='float32')
+            embedding = embedding_model.predict(mfcc_padded)[0]
+            embeddings.append(embedding)
+    speaker_embeddings[speaker] = np.mean(embeddings, axis=0)
+
