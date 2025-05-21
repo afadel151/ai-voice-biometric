5e0f7674ef3e68177c28334debfe68309b2b65df
Fadel Akram <af8356207@gmail.com>
2025-05-21 06:30:55 -0700

    train with cnn

--- speaker-id/train_cnn.py
+++ speaker-id/train_cnn.py
@@ -1,11 +1,9 @@
-from src.cnn.model import build_cnn_model
-from src.cnn.dataset import prepare_dataset
+from src.cnn.model import create_model as build_cnn_model
+from dataset import prepare_dataset
 from keras.api.preprocessing.sequence import pad_sequences
 
 from keras.api.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
-import os
-import numpy as np
 
 
 DATASET_DIR = "data"
@@ -14,3 +12,15 @@ le = LabelEncoder()
 y_encoded = le.fit_transform(y)
 y_cat = to_categorical(y_encoded)
 X_padded = pad_sequences(X, padding='post', dtype='float32')
+
+input_shape = X_padded.shape
+num_classes = y_cat.shape
+
+
+
+model = build_cnn_model((input_shape[1], input_shape[2], 1), num_classes[1])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_padded, y_cat, epochs=10, batch_size=32)
+
+model.save('cnn_speaker_model.keras')
