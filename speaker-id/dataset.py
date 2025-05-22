adding more layers to CNN model, fixed mfcc shape ,used class weight, , recall and precision metrics

speaker-id/dataset.py
@@ -1,9 +1,9 @@
 import os
-from src.mfcc import mfcc
 import sqlite3
+import librosa
 DATASET_DIR= "data"
 from src.audio_utils import load_and_preprocess
-
+import numpy as np
 def prepare_dataset(path=DATASET_DIR):
     X, y = [], []
     speakers = os.listdir(path)
@@ -16,8 +16,16 @@ def prepare_dataset(path=DATASET_DIR):
                 audio_path = os.path.join(audio_subdir, audio)
                 if audio.endswith('.flac'):
                     abs_path = os.path.abspath(audio_path)
+                    print(f"Processing file: {abs_path}")
                     y_audio, sr = load_and_preprocess(abs_path)
-                    mfcc_features = mfcc(signal=y_audio, sample_rate=sr)
+                    mfcc_features = librosa.feature.mfcc(signal=y_audio, sample_rate=sr)
+                    mfcc_features = librosa.feature.mfcc(y=y_audio, sr=sr, n_mfcc=40)
+                    if mfcc_features.shape[1] < 300:
+                        pad_width = 300 - mfcc_features.shape[1]
+                        mfcc_features = np.pad(mfcc_features, ((0, 0), (0, pad_width)), mode='constant')
+                    else:
+                        mfcc_features = mfcc_features[:, :300]
+                    mfcc_features = (mfcc_features - np.mean(mfcc_features)) / np.std(mfcc_features)
                     X.append(mfcc_features)
                     y.append(speaker_name)
     return X,y

speaker-id/src/mfcc.py
@@ -9,27 +9,40 @@ N_MFCC = 40
 SR = 16000 
 
 MEL_FILTERS = 128
-FRAME_LENGTH = 0.025
-FRAME_STEP = 0.01
+
 FFT_FILTERS = 2048
 
-def mfcc(signal, sample_rate=SR, frame_length=FRAME_LENGTH, frame_step=FRAME_STEP, num_mfcc=N_MFCC, num_mel_filters=MEL_FILTERS, target_frames=TARGET_FRAMES):
-    if not isinstance(signal, np.ndarray):
-        raise ValueError("Input signal must be a 1D numpy array.")
-    
-    S = power_to_db(mel_spectogram(signal, sr=sample_rate, n_fft=int(sample_rate * frame_length), hop_length=int(sample_rate * frame_step), n_mels=num_mel_filters))
-    M = dct(S, axis=-2, type=2, norm="ortho")[..., :num_mfcc, :]
-    
+def mfcc(signal, sample_rate=SR, num_mfcc=N_MFCC, num_mel_filters=MEL_FILTERS, target_frames=TARGET_FRAMES):
+    S = power_to_db(
+        mel_spectogram(signal, sample_r=sample_rate, n_fft=FFT_FILTERS, hop_length=512, n_mels=num_mel_filters)
+    )
+    M = dct(S, axis=-2, type=2, norm="ortho")[:num_mfcc, :]
     # Fixing the shape of M to (target_frames, num_mfcc)
-    if M.shape[0] > target_frames:
-        M = M[:target_frames, :]
-    elif M.shape[0] < target_frames:
-        pad_width = target_frames - mfcc.shape[0]
-        M = np.pad(mfcc, ((0, pad_width), (0, 0)), mode='constant')
+    current_frames = M.shape[1]
+    if current_frames > target_frames:
+        M = M[:, :target_frames]
+    elif current_frames < target_frames:
+        pad_width = target_frames - current_frames
+        M = np.pad(M, ((0, 0), (0, pad_width)), mode='constant')
     return M
    
    
-
+def spectogram(y, n_fft=2048, hop_length=512, win_length=None, center=True, window="hann", pad_mode="constant", power=1.0):
+     S = (
+            np.abs(
+                librosa.core.spectrum.stft(
+                    y,
+                    n_fft=n_fft,
+                    hop_length=hop_length,
+                    win_length=win_length,
+                    center=center,
+                    window=window,
+                    pad_mode=pad_mode,
+                )
+            )
+            ** power
+        )
+     return S,n_fft
  
 def compute_stft(y):
     if not isinstance(y, np.ndarray):
@@ -46,11 +59,14 @@ def power_spectogram(stft):
 
 
 def mel(sr: float,n_fft:int,n_mels = 128,fmin = 0.0,norm= "slaney"):
+    fmax = sr/2
     n_mels = int(n_mels)
     weights = np.zeros((n_mels, int(1 + n_fft // 2)), dtype=np.float32)
     fftfreqs = fft_frequencies(sr=sr, n_fft=n_fft)
-    mel_f = mel_frequencies(n_mels + 2, fmin=fmin)
+    mel_f = mel_frequencies(n_mels + 2, fmin=fmin,fmax=fmax)
+    
     fdiff = np.diff(mel_f)
+    
     ramps = np.subtract.outer(mel_f, fftfreqs)
     for i in range(n_mels):
         # lower and upper slopes for all bins
@@ -81,11 +97,11 @@ def fft_frequencies(*, sr = 22050, n_fft = 2048):
 
 
 def mel_frequencies(n_mels = 128, *, fmin = 0.0, fmax = 11025.0, htk = False):
-    min_mel = hz_to_mel(fmin, htk=htk)
-    max_mel = hz_to_mel(fmax, htk=htk)
+    min_mel = librosa.core.convert.hz_to_mel(fmin, htk=htk)
+    max_mel =  librosa.core.convert.hz_to_mel(fmax, htk=htk)
     mels = np.linspace(min_mel, max_mel, n_mels)
 
-    hz: np.ndarray = mel_to_hz(mels)
+    hz: np.ndarray =  librosa.core.convert.mel_to_hz(mels)
     return hz
 
 def hz_to_mel(frequencies, *, htk = False):
@@ -216,16 +232,11 @@ def tiny(x):
     return np.finfo(dtype).tiny
 
 
-def mel_spectogram(y, sr=16000, n_fft=2048, hop_length=512, n_mels=128, fmin=0.0, fmax=None):
-    if not isinstance(y, np.ndarray):
-        raise ValueError("Input signal must be a 1D numpy array.")
-    if fmax is None:
-        fmax = sr / 2
-    mel_basis = mel(sr=sr, n_fft=n_fft, n_mels=n_mels, fmin=fmin)
-    stft = compute_stft(y)
-    power_spectrogram = power_spectogram(stft)
-    mel_spectrogram = np.dot(mel_basis, power_spectrogram)
-    return mel_spectrogram
+def mel_spectogram(y, sample_r=16000, n_fft=2048, hop_length=512, n_mels=128, fmin=0.0, fmax=None):
+    S,n_fft = spectogram(y, n_fft, hop_length=hop_length)
+    mel_basis = mel(sample_r, n_fft, n_mels, fmin=fmin)
+    melspec: np.ndarray = np.einsum("...ft,mf->...mt", S, mel_basis, optimize=True)
+    return melspec
 
 def power_to_db(mel_spectrogram, ref=np.max):
     if not isinstance(mel_spectrogram, np.ndarray):

speaker-id/train_cnn.py
@@ -1,10 +1,11 @@
 from src.cnn.model import create_model as build_cnn_model
 from dataset import prepare_dataset
 from keras.api.preprocessing.sequence import pad_sequences
-
+from keras.api.metrics import Precision, Recall
 from keras.api.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
-
+from sklearn.utils import class_weight
+import numpy as np
 
 DATASET_DIR = "data"
 X,y = prepare_dataset(path=DATASET_DIR)
@@ -20,7 +21,9 @@ num_classes = y_cat.shape
 
 model = build_cnn_model((input_shape[1], input_shape[2], 1), num_classes[1])
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit(X_padded, y_cat, epochs=10, batch_size=32)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', Precision(), Recall()])
+class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_encoded), y=y_encoded)
+class_weights_dict = dict(enumerate(class_weights))
+model.fit(X_padded, y_cat, epochs=20, batch_size=32,class_weight=class_weights_dict)
 
 model.save('cnn_speaker_model.keras')