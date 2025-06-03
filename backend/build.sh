4c57ec93a7d0ffb2d8f24379fd3c0627eb23a618
Fadel Akram <af8356207@gmail.com>
2025-06-03 07:39:22 -0700

    moved binaries to bin

--- backend/build.sh
+++ backend/build.sh
@@ -2,18 +2,19 @@
 
 set -e
 
-# Install FFmpeg manually (Render doesn't have it by default)
-echo "Installing FFmpeg..."
-mkdir -p ffmpeg
-cd ffmpeg
+# Make a local bin directory for ffmpeg
+mkdir -p bin
+mkdir -p ffmpeg_build
+cd ffmpeg_build
 
-# Download static build of FFmpeg
+# Download FFmpeg static build
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
-
-# Extract and move ffmpeg binary to /usr/local/bin
 tar -xf ffmpeg.tar.xz --strip-components=1
-chmod +x ffmpeg
-mv ffmpeg /usr/local/bin/ffmpeg
-mv ffprobe /usr/local/bin/ffprobe
 
-echo "FFmpeg installed successfully."
+# Move binaries to bin/
+mv ffmpeg ../bin/ffmpeg
+mv ffprobe ../bin/ffprobe
+chmod +x ../bin/ffmpeg ../bin/ffprobe
+
+cd ..
+rm -rf ffmpeg_build
