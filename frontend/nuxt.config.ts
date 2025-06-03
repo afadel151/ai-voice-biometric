594400274be0fecb21fc2fc0b133ba7555d3a936
Fadel Akram <af8356207@gmail.com>
2025-06-03 08:14:49 -0700

    ssr: false

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-
+  ssr: false,
   vite: {
     plugins: [
       tailwindcss(),
